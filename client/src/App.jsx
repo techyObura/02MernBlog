7f387e4899f0164dd4204d1a4a06784d9797efa8
unknown <techyobura@gmail.com>
2025-11-23 23:40:51 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center text-9xl">
+      <h1 className="text-red-700">App</h1>
+    </div>
+  );
+};
+
+export default App;
